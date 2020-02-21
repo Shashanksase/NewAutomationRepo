b56bde15faabe150fc9b1726205c9c7e2c4c60f7
shashank <shashank.sase@gmail.com>
2020-02-21 10:20:30 +0530

    java21/2

---
+++ SeleniumProject/src/basicSelenium/BasicWebElement.java
@@ -0,0 +1,19 @@
+package basicSelenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicWebElement {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\drivers\\chromedriver.exe");
+		ChromeDriver dr = new ChromeDriver();
+		dr.manage().window().maximize();
+		dr.get("https://www.google.com/");
+		dr.quit();
+	}	
+	
+	
+	
+	
+	
+}
--- SeleniumProject/src/basicSelenium/FirstSeleniumClass.java
+++ SeleniumProject/src/basicSelenium/FirstSeleniumClass.java
@@ -1,5 +1,7 @@
 package basicSelenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstSeleniumClass {
@@ -8,12 +10,26 @@ public class FirstSeleniumClass {
 		System.setProperty("webdriver.chrome.driver", "D:\\drivers\\chromedriver.exe");
 		ChromeDriver dr = new ChromeDriver();
 		dr.manage().window().maximize();
-		dr.get("https://www.google.com/");
-		dr.quit();
-	}	
+		dr.get("http://sampleapp.tricentis.com/101/");
+		// WebElement automobile = dr.findElement(By.id("nav_automobile")); ---- By id
+		// WebElement automobile = dr.findElement(By.name("Navigation Automobile"));
+		// //// ------By name
+		// WebElement automobile = dr.findElement(By.linkText("Automobile"));// ---By
+		// Linktext
+		// WebElement automobile = dr.findElement(By.partialLinkText("Auto")); //-- By
+		// partiallinktext
+		// WebElement automobile =
+		// dr.findElement(By.xpath("//a[@id='nav_automobile']"));// xpath
+
+		//WebElement automobile = dr.findElement(By.cssSelector("a#nav_automobile"));
+		WebElement automobile = dr.findElement(By.id("nav_automobile"));
+		String actual = automobile.getText();
+		System.out.println(actual);
 	
-	
-	
-	
-	
+		automobile.click();
+		dr.findElement(By.xpath("//input[@id='engineperformance']")).sendKeys("1200");
+
+		// dr.quit();
+	}
+
 }
